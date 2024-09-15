ddf2c7712d79f92cba6fb91d17adadd9d43c9b47
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-09-15 14:59:43 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,35 +5,15 @@ const list = document.querySelector('.gallery')
 
 galleryItems.map(item => {
     list.insertAdjacentHTML('beforeend', `<li class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
 </li>`)
 });
 
 
-list.addEventListener('click', onClick)
-
-function onClick(evt) {
-    evt.preventDefault();
-
-    const imgUrl = evt.target.dataset.source
-    const instance = basicLightbox.create(`<img src="${imgUrl}" width="800" height="600">`)
-
-    instance.show()
-
-    document.addEventListener('keydown', onPress)
-
-    function onPress(evt) {
-        if (evt.code === "Escape") {
-            instance.close()
-            document.removeEventListener('keydown', onPress)
-        }
-    }
-}
-
+new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250
+ });
+   
